6b5b12bb3d337a033465d684eb346f4ea2c242bb
Gabriel Duarte <gabrielduarte@claretiano.edu.br>
2021-03-09 11:12:24 -0300

    Alteração no arquivo index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ module.exports = {
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
-        "project": "tsconfig.json",
+        "project": ["tsconfig.json", "test/tsconfig.json"],
         "sourceType": "module"
     },
     "plugins": [
